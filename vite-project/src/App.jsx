b7948163d2e3f72a968a340455ff4f4ce64f3393
Mohammed Abubaker <m.abubaker.eld@gmail.com>
2026-03-24 13:32:47 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Cards from './assets/cards';
+
+
+
+function App() {
+
+   return (<Cards/>);
+}
+
+export default App
---
+++ vite-project/src/assets/cards.jsx
@@ -0,0 +1,14 @@
+
+function Cards(){
+    const name = "Abubaker";
+
+    return(<>
+           <div className="card">
+            <img className="img" src="" alt="profile pic" />
+            <h2>Name:{name}</h2>
+            <p>description : i am a programmer</p>
+            <p>hobbies : play games</p>
+            </div>   </>
+    );
+}
+export default Cards
